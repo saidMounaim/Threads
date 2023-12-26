466b8819326508effc29aa94b1828b3c9a8df688
saidMounaim <saidmounaim00@gmail.com>
2023-12-26 15:58:38 +0100

    Fetch latest Threads

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,16 +2,48 @@ import { redirect } from "next/navigation";
 import { authOptions } from "../utils/auth";
 import { getServerSession } from "next-auth";
 import ThreadsCard from "../components/ThreadsCard";
+import prisma from "../utils/db";
+
+async function getData() {
+  const data = await prisma.thread.findMany({
+    select: {
+      id: true,
+      description: true,
+      user: {
+        select: {
+          id: true,
+          name: true,
+          image: true,
+        },
+      },
+      createdAt: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return data;
+}
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
   if (!session) return redirect("/login");
 
+  const threads = await getData();
+
   return (
-    <section className="max-w-4xl mt-11 flex flex-col gap-10 ml-16">
+    <section className="max-w-4xl mt-11 flex flex-col gap-5 ml-16">
       <h1 className="text-3xl font-bold text-white">Latest Threads</h1>
-      <ThreadsCard />
+      {threads.map((thread) => (
+        <ThreadsCard
+          key={thread.id}
+          id={thread.id}
+          description={thread.description}
+          createdAt={thread.createdAt}
+          user={thread.user}
+        />
+      ))}
     </section>
   );
 }
--- app/components/ThreadsCard.tsx
+++ app/components/ThreadsCard.tsx
@@ -1,19 +1,34 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
-const ThreadsCard = () => {
+type TUser = {
+  id: string | null;
+  name: string | null;
+  image: string | null;
+};
+
+interface IThreadsCard {
+  id: string;
+  description: string;
+  user: TUser;
+  createdAt: Date;
+}
+
+const ThreadsCard = ({ id, description, user, createdAt }: IThreadsCard) => {
   return (
-    <div className="flex w-full flex-col rounded-xl bg-slate-900 p-7">
+    <div className="flex w-full flex-col rounded-xl bg-slate-900 p-7" key={id}>
       <div className="flex items-start gap-x-4">
         <div className="flex items-center">
           <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
+            {user?.image ? (
+              <AvatarImage src={user?.image} />
+            ) : (
+              <AvatarImage src={"https://github.com/shadcn.png"} />
+            )}
           </Avatar>
         </div>
         <div className="flex flex-col gap-y-2">
-          <h2 className="text-lg text-white font-bold">Said MOUNAIM</h2>
-          <p className="text-md font-normal text-white">
-            This is a test of the Threads app!
-          </p>
+          <h2 className="text-lg text-white font-bold">{user?.name}</h2>
+          <p className="text-md font-normal text-white">{description}</p>
         </div>
       </div>
     </div>
