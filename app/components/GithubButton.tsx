0a411dcb5b09d8b3d72a65ac664f9ad1138bc348
saidMounaim <saidmounaim00@gmail.com>
2023-12-26 11:00:42 +0100

    Install & Setup Next-Auth

--- app/components/GithubButton.tsx
+++ app/components/GithubButton.tsx
@@ -2,11 +2,15 @@
 
 import { Button } from "@/components/ui/button";
 import { GithubIcon } from "lucide-react";
+import { signIn } from "next-auth/react";
 import React from "react";
 
 const GithubButton = () => {
   return (
-    <Button className="flex items-center justify-center gap-x-3 mt-4">
+    <Button
+      onClick={() => signIn("github")}
+      className="flex items-center justify-center gap-x-3 mt-4"
+    >
       <GithubIcon className="w-5 h-5" />
       <p className="font-medium text-md">Continue with Github</p>
     </Button>
