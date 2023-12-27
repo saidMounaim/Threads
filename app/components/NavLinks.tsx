93f084ef1856d67d0825386d31248c4feebff390
saidMounaim <saidmounaim00@gmail.com>
2023-12-27 10:10:24 +0100

    Add class active on nav links component

---
+++ app/components/NavLinks.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { HomeIcon, ImageIcon, SearchIcon, User2Icon } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavLinks = () => {
+  const pathname = usePathname();
+
+  return (
+    <ul className="flex gap-y-5 flex-col items-start mt-9">
+      <li
+        className={`flex flex-col py-3 px-4 rounded-lg w-full ${
+          pathname == "/" ? `bg-blue-600` : ``
+        }`}
+      >
+        <Link
+          href="/"
+          className="flex gap-x-2 items-center font-medium text-xl"
+        >
+          <HomeIcon className="w-5 h-5" />
+          Home
+        </Link>
+      </li>
+      <li
+        className={`flex flex-col py-3 px-4 rounded-lg w-full ${
+          pathname == "/search" ? `bg-blue-600` : ``
+        }`}
+      >
+        <Link
+          href="/search"
+          className="flex gap-x-2 items-center font-medium text-xl"
+        >
+          <SearchIcon className="w-5 h-5" />
+          Search
+        </Link>
+      </li>
+      <li
+        className={`flex flex-col py-3 px-4 rounded-lg w-full ${
+          pathname == "/create" ? `bg-blue-600` : ``
+        }`}
+      >
+        <Link
+          href="/create"
+          className="flex gap-x-2 items-center font-medium text-xl"
+        >
+          <ImageIcon className="w-5 h-5" />
+          Create Thread
+        </Link>
+      </li>
+      <li
+        className={`flex flex-col py-3 px-4 rounded-lg w-full ${
+          pathname == "/user/profile" ? `bg-blue-600` : ``
+        }`}
+      >
+        <Link
+          href="/"
+          className="flex gap-x-2 items-center font-medium text-xl"
+        >
+          <User2Icon className="w-5 h-5" />
+          Profile
+        </Link>
+      </li>
+    </ul>
+  );
+};
+
+export default NavLinks;
--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,7 +1,6 @@
-import { HomeIcon, ImageIcon, SearchIcon, User2Icon } from "lucide-react";
-import Link from "next/link";
 import React from "react";
 import LogoutButton from "./LogoutButton";
+import NavLinks from "./NavLinks";
 
 const SideBar = () => {
   return (
@@ -9,44 +8,7 @@ const SideBar = () => {
       <div className="flex flex-col py-4 px-11">
         <h1 className="text-white font-bold text-4xl">Threads</h1>
 
-        <ul className="flex gap-y-5 flex-col items-start mt-9">
-          <li className="flex flex-col bg-blue-600 py-3 px-4 rounded-lg w-full">
-            <Link
-              href="/"
-              className="flex gap-x-2 items-center font-medium text-xl"
-            >
-              <HomeIcon className="w-5 h-5" />
-              Home
-            </Link>
-          </li>
-          <li className="flex flex-col py-3 px-4 rounded-lg w-full">
-            <Link
-              href="/search"
-              className="flex gap-x-2 items-center font-medium text-xl"
-            >
-              <SearchIcon className="w-5 h-5" />
-              Search
-            </Link>
-          </li>
-          <li className="flex flex-col py-3 px-4 rounded-lg w-full">
-            <Link
-              href="/create"
-              className="flex gap-x-2 items-center font-medium text-xl"
-            >
-              <ImageIcon className="w-5 h-5" />
-              Create Thread
-            </Link>
-          </li>
-          <li className="flex flex-col py-3 px-4 rounded-lg w-full">
-            <Link
-              href="/"
-              className="flex gap-x-2 items-center font-medium text-xl"
-            >
-              <User2Icon className="w-5 h-5" />
-              Profile
-            </Link>
-          </li>
-        </ul>
+        <NavLinks />
       </div>
       <div className="p-4 w-full flex">
         <LogoutButton />
