b63657741523bc8d1b2edeb7d0e886b5981e186b
saidMounaim <saidmounaim00@gmail.com>
2023-12-28 11:16:29 +0100

    Add User Profile Public for all users

---
+++ app/(root)/user/[id]/page.tsx
@@ -0,0 +1,112 @@
+import ThreadsCard from "@/app/components/ThreadsCard";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import prisma from "../../../utils/db";
+import { Button } from "@/components/ui/button";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/utils/auth";
+
+async function getData(userId: string) {
+  const data = await prisma.user.findUnique({
+    where: { id: userId },
+    select: {
+      id: true,
+      name: true,
+      image: true,
+      Threads: {
+        select: {
+          id: true,
+          description: true,
+          createdAt: true,
+          Comments: {
+            select: {
+              id: true,
+              comment: true,
+              userId: true,
+              threadId: true,
+              createdAt: true,
+            },
+          },
+          Likes: {
+            select: {
+              id: true,
+              userId: true,
+              threadId: true,
+              createdAt: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  return data;
+}
+
+interface IProfilePage {
+  params: {
+    id: string;
+  };
+}
+
+const ProfilePage = async ({ params }: IProfilePage) => {
+  const id = params.id;
+
+  const session = await getServerSession(authOptions);
+
+  const user = await getData(id);
+
+  if (session?.user.id === user?.id) return redirect("/user/profile");
+
+  if (!user) return redirect("/");
+
+  return (
+    <section className="max-w-4xl mt-11 flex flex-col gap-5 ml-16">
+      <div className="flex gap-x-9">
+        <div className="flex items-center">
+          {user.image && (
+            <Image
+              src={user.image}
+              alt="Avatar"
+              width={90}
+              height={90}
+              className="rounded-full"
+            />
+          )}
+        </div>
+        <div className="flex flex-col gap-y-2">
+          <h2 className="text-3xl text-white font-bold">{user.name}</h2>
+          <div className="flex gap-x-3">
+            <p>{user?.Threads.length} Threads</p>
+            <p>0 Followers</p>
+            <p>0 Following</p>
+          </div>
+          <div className="flex flex-col mt-3">
+            <Button
+              variant="destructive"
+              className="bg-blue-600 text-white hover:bg-blue-800 transition"
+            >
+              Follow
+            </Button>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-3xl font-bold text-white mt-32">
+        {user.name} Threads
+      </h1>
+      {user?.Threads.map((thread) => (
+        <ThreadsCard
+          key={thread.id}
+          id={thread.id}
+          description={thread.description}
+          createdAt={thread.createdAt}
+          user={user}
+          comments={thread.Comments}
+          likes={thread.Likes}
+          hideComment={false}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default ProfilePage;
--- app/components/ThreadsCard.tsx
+++ app/components/ThreadsCard.tsx
@@ -54,7 +54,7 @@ const ThreadsCard = ({
       key={id}
     >
       <Link href={`/thread/${id}`} className="flex items-start gap-x-4">
-        <div className="flex items-center">
+        <Link href={`/user/${user?.id}`} className="flex items-center">
           <Avatar>
             {user?.image ? (
               <AvatarImage src={user?.image} />
@@ -62,7 +62,7 @@ const ThreadsCard = ({
               <AvatarImage src={"https://github.com/shadcn.png"} />
             )}
           </Avatar>
-        </div>
+        </Link>
         <div className="flex flex-col gap-y-2">
           <h2 className="text-lg text-white font-bold">{user?.name}</h2>
           <p className="text-md font-normal text-white">{description}</p>
